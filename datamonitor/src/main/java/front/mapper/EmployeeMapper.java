37ebfcdacd52b1e43e7920bd865496e5dffcaa44
hxl <1926972815@qq.com>
2018-07-17 20:10:02 +0800

    提交

---
+++ datamonitor/src/main/java/front/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package front.mapper;
+
+import front.entity.Employee;
+import java.util.List;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Employee record);
+
+    Employee selectByPrimaryKey(Integer id);
+
+    List<Employee> selectAll();
+
+    int updateByPrimaryKey(Employee record);
+}
